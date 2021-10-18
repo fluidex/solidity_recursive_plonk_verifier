3d12d7948e47cff8f0bf9b656bb6921555f52e09
HAOYUatHZ <haoyu@protonmail.com>
2021-10-18 16:56:41 +0800

    move

--- src/VerifierTemplate.sol
+++ src/VerifierTemplate.sol
@@ -9,118 +9,6 @@ pragma experimental ABIEncoderV2;
 // TODO: chunks?
 // import "./PlonkCore.sol";
 
-// Hardcoded constants to avoid accessing store
-contract KeysWithPlonkVerifier is VerifierWithDeserialize {
-
-    uint256 constant VK_TREE_ROOT = {{vk_tree_root}};
-    uint8 constant VK_MAX_INDEX = 255; // hardcoded as 255 to consist with plonkit
-    uint256 internal constant INPUT_MASK = $$(~uint256(0) >> 3);
-
-    function getVkAggregated() internal pure returns(VerificationKey memory vk) {
-        vk.domain_size = {{domain_size}};
-        vk.num_inputs = {{num_inputs}};
-        vk.omega = PairingsBn254.new_fr({{omega}});
-        vk.gate_setup_commitments[0] = PairingsBn254.new_g1(
-            {{gate_setup_commitment_0_0}},
-            {{gate_setup_commitment_0_1}}
-        );
-        vk.gate_setup_commitments[1] = PairingsBn254.new_g1(
-            {{gate_setup_commitment_1_0}},
-            {{gate_setup_commitment_1_1}}
-        );
-        vk.gate_setup_commitments[2] = PairingsBn254.new_g1(
-            {{gate_setup_commitment_2_0}},
-            {{gate_setup_commitment_2_1}}
-        );
-        vk.gate_setup_commitments[3] = PairingsBn254.new_g1(
-            {{gate_setup_commitment_3_0}},
-            {{gate_setup_commitment_3_1}}
-        );
-        vk.gate_setup_commitments[4] = PairingsBn254.new_g1(
-            {{gate_setup_commitment_4_0}},
-            {{gate_setup_commitment_4_1}}
-        );
-        vk.gate_setup_commitments[5] = PairingsBn254.new_g1(
-            {{gate_setup_commitment_5_0}},
-            {{gate_setup_commitment_5_1}}
-        );
-        vk.gate_setup_commitments[6] = PairingsBn254.new_g1(
-            {{gate_setup_commitment_6_0}},
-            {{gate_setup_commitment_6_1}}
-        );
-        vk.gate_selector_commitments[0] = PairingsBn254.new_g1(
-            {{gate_selector_commitment_0_0}},
-            {{gate_selector_commitment_0_1}}
-        );
-        vk.gate_selector_commitments[1] = PairingsBn254.new_g1(
-            {{gate_selector_commitment_1_0}},
-            {{gate_selector_commitment_1_1}}
-        );
-        vk.copy_permutation_commitments[0] = PairingsBn254.new_g1(
-            {{permutation_commitment_0_0}},
-            {{permutation_commitment_0_1}}
-        );
-        vk.copy_permutation_commitments[1] = PairingsBn254.new_g1(
-            {{permutation_commitment_1_0}},
-            {{permutation_commitment_1_1}}
-        );
-        vk.copy_permutation_commitments[2] = PairingsBn254.new_g1(
-            {{permutation_commitment_2_0}},
-            {{permutation_commitment_2_1}}
-        );
-        vk.copy_permutation_commitments[3] = PairingsBn254.new_g1(
-            {{permutation_commitment_3_0}},
-            {{permutation_commitment_3_1}}
-        );
-        vk.copy_permutation_non_residues[0] = PairingsBn254.new_fr(
-            {{permutation_non_residue_0}}
-        );
-        vk.copy_permutation_non_residues[1] = PairingsBn254.new_fr(
-            {{permutation_non_residue_1}}
-        );
-        vk.copy_permutation_non_residues[2] = PairingsBn254.new_fr(
-            {{permutation_non_residue_2}}
-        );
-
-        vk.g2_x = PairingsBn254.new_g2(
-            [{{g2_x_x_c1}},
-            {{g2_x_x_c0}}],
-            [{{g2_x_y_c1}},
-            {{g2_x_y_c0}}]
-        );
-    }
-
-    function verifyAggregatedProof(
-        uint256[] memory _recursiveInput,
-        uint256[] memory _proof,
-        uint8[] memory _vkIndexes,
-        uint256[] memory _individualVksInputs,
-        uint256[16] memory _subproofsLimbs
-    ) external view returns (bool) {
-
-        /*
-        for (uint256 i = 0; i < _individualVksInputs.length; ++i) {
-            uint256 commitment = _individualVksInputs[i];
-            _individualVksInputs[i] = commitment & INPUT_MASK;
-        }
-        */
-
-        VerificationKey memory vk = getVkAggregated();
-
-        return
-            verify_serialized_proof_with_recursion(
-                _recursiveInput,
-                _proof,
-                VK_TREE_ROOT,
-                VK_MAX_INDEX,
-                _vkIndexes,
-                _individualVksInputs,
-                _subproofsLimbs,
-                vk
-            );
-    }
-}
-
 library PairingsBn254 {
     uint256 constant q_mod = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
     uint256 constant r_mod = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
@@ -1317,5 +1205,114 @@ contract VerifierWithDeserialize is Plonk4VerifierWithAccessToDNext {
             );
 
         return valid;
+    }
+}
+
+// Hardcoded constants to avoid accessing store
+contract KeysWithPlonkVerifier is VerifierWithDeserialize {
+
+    uint256 constant VK_TREE_ROOT = {{vk_tree_root}};
+    uint8 constant VK_MAX_INDEX = 255; // hardcoded as 255 to consist with plonkit
+    uint256 internal constant INPUT_MASK = ~uint256(0) >> 3;
+
+    function getVkAggregated() internal pure returns(VerificationKey memory vk) {
+        vk.domain_size = {{domain_size}};
+        vk.num_inputs = {{num_inputs}};
+        vk.omega = PairingsBn254.new_fr({{omega}});
+        vk.gate_setup_commitments[0] = PairingsBn254.new_g1(
+            {{gate_setup_commitment_0_0}},
+            {{gate_setup_commitment_0_1}}
+        );
+        vk.gate_setup_commitments[1] = PairingsBn254.new_g1(
+            {{gate_setup_commitment_1_0}},
+            {{gate_setup_commitment_1_1}}
+        );
+        vk.gate_setup_commitments[2] = PairingsBn254.new_g1(
+            {{gate_setup_commitment_2_0}},
+            {{gate_setup_commitment_2_1}}
+        );
+        vk.gate_setup_commitments[3] = PairingsBn254.new_g1(
+            {{gate_setup_commitment_3_0}},
+            {{gate_setup_commitment_3_1}}
+        );
+        vk.gate_setup_commitments[4] = PairingsBn254.new_g1(
+            {{gate_setup_commitment_4_0}},
+            {{gate_setup_commitment_4_1}}
+        );
+        vk.gate_setup_commitments[5] = PairingsBn254.new_g1(
+            {{gate_setup_commitment_5_0}},
+            {{gate_setup_commitment_5_1}}
+        );
+        vk.gate_setup_commitments[6] = PairingsBn254.new_g1(
+            {{gate_setup_commitment_6_0}},
+            {{gate_setup_commitment_6_1}}
+        );
+        vk.gate_selector_commitments[0] = PairingsBn254.new_g1(
+            {{gate_selector_commitment_0_0}},
+            {{gate_selector_commitment_0_1}}
+        );
+        vk.gate_selector_commitments[1] = PairingsBn254.new_g1(
+            {{gate_selector_commitment_1_0}},
+            {{gate_selector_commitment_1_1}}
+        );
+        vk.copy_permutation_commitments[0] = PairingsBn254.new_g1(
+            {{permutation_commitment_0_0}},
+            {{permutation_commitment_0_1}}
+        );
+        vk.copy_permutation_commitments[1] = PairingsBn254.new_g1(
+            {{permutation_commitment_1_0}},
+            {{permutation_commitment_1_1}}
+        );
+        vk.copy_permutation_commitments[2] = PairingsBn254.new_g1(
+            {{permutation_commitment_2_0}},
+            {{permutation_commitment_2_1}}
+        );
+        vk.copy_permutation_commitments[3] = PairingsBn254.new_g1(
+            {{permutation_commitment_3_0}},
+            {{permutation_commitment_3_1}}
+        );
+        vk.copy_permutation_non_residues[0] = PairingsBn254.new_fr(
+            {{permutation_non_residue_0}}
+        );
+        vk.copy_permutation_non_residues[1] = PairingsBn254.new_fr(
+            {{permutation_non_residue_1}}
+        );
+        vk.copy_permutation_non_residues[2] = PairingsBn254.new_fr(
+            {{permutation_non_residue_2}}
+        );
+
+        vk.g2_x = PairingsBn254.new_g2(
+            [{{g2_x_x_c1}},
+            {{g2_x_x_c0}}],
+            [{{g2_x_y_c1}},
+            {{g2_x_y_c0}}]
+        );
+    }
+
+    function verifyAggregatedProof(
+        uint256[] memory _recursiveInput,
+        uint256[] memory _proof,
+        uint8[] memory _vkIndexes,
+        uint256[] memory _individualVksInputs,
+        uint256[16] memory _subproofsLimbs
+    ) external view returns (bool) {
+        for (uint256 i = 0; i < _individualVksInputs.length; ++i) {
+            uint256 commitment = _individualVksInputs[i];
+            _individualVksInputs[i] = commitment & INPUT_MASK;
+        }
+
+        VerificationKey memory vk = getVkAggregated();
+
+        return
+            verify_serialized_proof_with_recursion(
+                _recursiveInput,
+                _proof,
+                VK_TREE_ROOT,
+                VK_MAX_INDEX,
+                _vkIndexes,
+                _individualVksInputs,
+                _subproofsLimbs,
+                vk
+            );
     }
 }
